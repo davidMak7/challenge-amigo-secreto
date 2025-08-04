5c3e06352c0c844d99bca84681a22ab2c12912ed
davidMak7 <crownrampage@gmail.com>
2025-08-03 23:09:30 -0300

    Actualize app.js con mejoras en la lógica

--- app.js
+++ app.js
@@ -1,39 +1,65 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
  
 let listaAmigos = [ ];
+let cantidadAmigos = 5;
+
 function agregarAmigo() {
   // Tu código aquí
-  let amigo = document.getElementById('amigo').value;
-  if (typeof amigo === 'string' && amigo.trim() !== '') {
-    listaAmigos.push(amigo.trim());
-    console.log(`Amigo ${amigo} agregado correctamente.`);
-  }
-  mostrarAmigos();
+     document.getElementById('resultado').innerText = '';
+    if (listaAmigos.length >= cantidadAmigos) { 
+        alert(`No puedes agregar más de ${cantidadAmigos} amigos.`);
+        document.getElementById('amigo').value = ''; // Limpiar el campo de entrada
+        return;
+    }else{
+    let amigo = document.getElementById('amigo').value;
+        if (typeof amigo === 'string' && amigo.trim() !== '') {
+            // Verificar si el amigo ya está en la lista
+            if (listaAmigos.includes(amigo.trim())) {
+                alert(`El amigo ${amigo} ya está en la lista.`);
+                return;
+            }else {
+            listaAmigos.push(amigo.trim());
+            }
+        }
+    }
+    actualizarLista();
+    document.getElementById('amigo').value = ''; // Limpiar el campo de entrada
+    mostrarAmigos();
 }
 
 function mostrarAmigos() {
   // Tu código aquí
-  const lista = document.getElementById('listaAmigos');
-  lista.innerHTML = ''; // Limpiar la lista antes de mostrar los amigos
+  
   if (listaAmigos.length === 0) {
-
-    console.log('No tienes amigos.');
+    alert(`No tienes amigos. Agrega algunos amigos para comenzar.`);
   } else {
-    console.log('Tus amigos son:');
     listaAmigos.forEach((amigo, index) => {
     document.getElementById('listaAmigos').innerHTML += `<li>${index + 1}. ${amigo}</li>`;
     })
   };
 }
+ 
+function actualizarLista() { 
+    // Tu código aquí
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ''; // actualiza la lista antes de mostrar los amigos
+}
+
+function limpiarLista() {
+  // Tu código aquí
+    listaAmigos = [];   
+    actualizarLista();
+}
+
 
 function sortearAmigo() {
   // Tu código aquí
-  if (listaAmigos.length === 0) {
-    console.log('No hay amigos para sortear.');
-    return;
-  }
+    if (listaAmigos.length === 0) {
+        alert(`No tienes amigos. Agrega algunos amigos para comenzar.`);
+        return;
+    }
     const indiceAleatorio = Math.floor(Math.random() * listaAmigos.length);
     const amigoSorteado = listaAmigos[indiceAleatorio];
-    console.log(`El amigo sorteado es: ${amigoSorteado}`);
     document.getElementById('resultado').innerText = `El amigo sorteado es: ${amigoSorteado}`;
+    limpiarLista();
 }   
